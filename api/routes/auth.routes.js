4091a51bb8406d5544182ed874697ba06579cf64
juampi <jpabloamigo.m@gmail.com>
2023-10-15 19:24:58 -0300

    complete sign in page functionality

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -4,6 +4,6 @@ import { signup, singIn } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/signup', signup)
-router.post('/signip', singIn)
+router.post('/signin', singIn)
 
 export default router
